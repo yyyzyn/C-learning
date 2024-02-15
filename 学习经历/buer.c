b948dda39c895bf78dfae941122e73f786c6c148
仲启翔 <87054720+yyyzyn@users.noreply.github.com>
2024-02-15 11:05:36 +0800

    Add files via upload

---
+++ 学习经历/buer.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+//布尔字符串类型练习
+int main()
+{
+	_Bool flag = true;
+	bool  flag2;//布尔字符串类型第二种写法
+	if (flag)
+	{
+		printf("hehe\n");
+	}
+	return 0;
+
+}
---
+++ 学习经历/zhuanyi.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+//关于输出字符串相关内容
+//字符串输出大小问题
+
+
+int main()
+{
+	int len = strlen("abc");
+	printf("%d/n",len);
+	return 0;
+}
